3268dabac12a75ceff6052c834d6195d33204373
José Pedro Dias <jose.pedro.dias@gmail.com>
2021-03-07 17:51:40 +0000

    getting somewhere: mapping tile positions

--- bevy_dev/src/bin/quad_uvs.rs
+++ bevy_dev/src/bin/quad_uvs.rs
@@ -7,6 +7,8 @@ use bevy::{
     }
 };
 
+struct MainCamera;
+
 struct Rotator;
 
 fn rotator_system(time: Res<Time>, mut query: Query<&mut Transform, With<Rotator>>) {
@@ -21,27 +23,88 @@ struct Translator {
     tr: Vec3,
 }
 
+struct TileIndex {
+    index: usize
+}
+
 fn si(n:f32, scale:f32, period:f32, shift:f32) -> f32 {
     ((n * period) % 3.1415927).sin() * scale + shift
 }
 
-fn translator_system(time: Res<Time>, mut query: Query<(&Translator, &mut Transform), With<Translator>>) {
+fn translator_system(
+    time: Res<Time>,
+    mut query: Query<(&Translator, &mut Transform), With<Translator>>
+) {
     //let dt = time.delta_seconds();
     let t = time.seconds_since_startup() as f32;
     for (i, (translator, mut transform)) in query.iter_mut().enumerate() {
         let tr = translator.tr;
-        let dti1 = (i as f32) * 0.025;
-        let dti2 = 1.0 + (i as f32) * 0.025;
+        let _dti1 = (i as f32) * 0.025;
+        let _dti2 = 1.0 + (i as f32) * 0.025;
         transform.translation = Vec3::new(
             tr[0] + si(
-                t + dti1, // + dti1,
-                100.,// * dti2,
+                t + _dti1, // + _dti1,
+                100.,// * _dti2,
                 2.,
                 -50.
             ),
             tr[1],
             tr[2]
         );
+    }
+}
+
+fn my_cursor_system(
+    // events to get cursor position
+    ev_cursor: Res<Events<CursorMoved>>,
+    mut evr_cursor: Local<EventReader<CursorMoved>>,
+    // need to get window dimensions
+    wnds: Res<Windows>,
+    // query to get camera transform
+    q_camera: Query<&Transform, With<MainCamera>>,
+    q_tile_index: Query<(&TileIndex, &Translator), With<TileIndex>>
+) {
+    // assuming there is exactly one main camera entity, so this is OK
+    let camera_transform = q_camera.iter().next().unwrap();
+
+    let mut pos_wld:Option<Vec4> = None;
+
+    for ev in evr_cursor.iter(&ev_cursor) {
+        // get the size of the window that the event is for
+        let wnd = wnds.get(ev.id).unwrap();
+        let size = Vec2::new(wnd.width() as f32, wnd.height() as f32);
+        //println!("size: {} x {}", size.x, size.y);
+
+        // the default orthographic projection is in pixels from the center. just undo the translation
+        let p = ev.position - size / 2.0;
+
+        // apply the camera transform
+        pos_wld = Some(camera_transform.compute_matrix() * p.extend(0.0).extend(1.0));
+    }
+
+    if pos_wld.is_some() {
+        let pw = pos_wld.unwrap();
+        //eprintln!("World coords: {:.0} x {:.0}", pw.x, pw.y);
+
+        let mut nearest_ti = 0;
+        let mut nearest_dist:f32 = std::f32::MAX;
+
+        for (tile_index, translator) in q_tile_index.iter() {
+            let ti = tile_index.index;
+            let tr = translator.tr;
+            //println!("{:?} {:?}", ti, tr);
+
+            let dx = tr.x - pw.x;
+            let dy = tr.y - pw.y;
+            let dist = dx*dx + dy*dy;
+
+            if dist < nearest_dist {
+                nearest_dist = dist;
+                nearest_ti = ti;
+            }
+        }
+
+        println!("tile #{:?}", nearest_ti);
     }
 }
 
@@ -58,7 +121,8 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup.system())
         .add_system(translator_system.system())
-        //.add_system(rotator_system.system())
+        .add_system(rotator_system.system())
+        .add_system(my_cursor_system.system())
         .add_system(bevy::input::system::exit_on_esc_system.system())
         .run();
 }
@@ -73,39 +137,46 @@ fn setup(
     let img_w = 2048.;
     let img_h = 1135.;
     let s = 0.33;
-    let w = s;
-    let h = s;
+    let w = img_w * s;
+    let h = img_h * s;
     
 
-    commands.spawn(Camera2dBundle::default());
+    commands.spawn(Camera2dBundle::default())
+        .with(MainCamera);
 
     const W:usize = 4;
     const H:usize = 3;
 
     let du = 1.0 / W as f32;
     let dv = 1.0 / H as f32;
+    let mut ti:usize = 0;
     for ih in 0..H {
         for iw in 0..W {
             let u0 = (iw as f32) * 1.0 / (W as f32);
             let v0 = (ih as f32) * 1.0 / (H as f32);
             let translation = Vec3::new(
-                //0.,
-                //0.,
-                (-0.5 + ((iw as f32) + 0.5) * du) * w * img_w,
-                (0.5 - ((ih as f32) + 0.5) * dv) * h * img_h,
+                (-0.5 + ((iw as f32) + 0.5) * du) * w,
+                (0.5 - ((ih as f32) + 0.5) * dv) * h,
                 0.
             );
             commands.spawn(SpriteBundle {
                 mesh: meshes.add(build_quad_uvs(w*du, h*dv, u0, u0 + du, v0, v0 + dv)),
-                //mesh: meshes.add(build_quad_uvs(w, h, 0., 1., 0., 1.)),
                 material: materials.add(img_tex.clone().into()),
-                //sprite: Sprite::new(Vec2::new(s*du*ar, s*dv)),
-                //sprite: Sprite::new(Vec2::new(1., 1.)),
+                sprite: Sprite {
+                    //size: Vec2::new(w * du, h * dv),
+                    size: Vec2::new(1., 1.),
+                    resize_mode: SpriteResizeMode::Manual
+                    //resize_mode: SpriteResizeMode::Automatic
+                },
                 transform: Transform::from_translation(translation.clone()),
                 ..Default::default()
             })
+            .with(TileIndex { index: ti })
             //.with(Rotator);
             .with(Translator { tr: translation });
+            //println!("{}x{} ... {}", w*du, h*dv, translation[0]);
+
+            ti += 1;
         }
     }
 }
